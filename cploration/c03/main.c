426066588840cbe923fcac92597edc6c53fc49d7
Ivan Young <ivanyoung10@gmail.com>
2023-10-29 12:21:20 -0700

    Initial commit.

---
+++ cploration/c03/main.c
@@ -0,0 +1,15 @@
+/****************************************
+ * C-ploration 3 for CS 271
+ * 
+ * [NAME] $YOUR_NAME$
+ * [TERM] FALL $YEAR$
+ * 
+ ****************************************/
+ #include <stdio.h>
+ #include <string.h>
+ #include <stdlib.h>
+
+int main() {
+	printf("Hello World;")
+	return 0;
+}
